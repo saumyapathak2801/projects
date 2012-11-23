04b0f81dada34cd0813266e1654385b6b52c1910
saumyapathak2801 <saumyapathak2011@gmail.com>
2012-11-23 20:06:19 +0530

    Revert "Database Management system"
    This reverts commit acb316d1b45b96674524b68000af47d80d9d81aa.

--- db/td.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-session_start();
-require("res/config.php");
-echo $_GET['id'];
-?>
--- db/update.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-session_start();
-require("res/config.php");
-header("Location:update_".$_GET['id'].".php");
-?>
